2fb67032f0997b8b10367a505cd4ff4ab827f3f5
awqx <alisonwxin@gmail.com>
2018-06-19 12:08:57 -0400

    Added steps up to PaDEL-Descriptor

--- cdap.R
+++ cdap.R
@@ -2,7 +2,39 @@
 # Setting up --------------------------------------------------------------
 
 getwd()
+source("cactus.R")
+source("applicability.domain.R")
+source("qsar.R")
 
 # Downloading SDFs --------------------------------------------------------
 
+#     Names ----
+# Either write out the names of the guest molecules you want...
+guest <- c("example1", "example2")
 
+# Or read them from a .csv
+guests <- read.csv("guests.csv", header = T) %>% .$guests
+
+#     Structural files with CACTUS ----
+
+# Create a folder to hold the molecules
+dir.create("guests")
+
+# Download only a single guest molecule
+results.dwnld <- download.cactus.results(guest, path = "guests", 
+                                         chemical.format = "SDF")
+
+# Download multiple guest molecules
+results.dwnld <-
+  do.call(
+    rbind,
+    lapply(
+      guest,
+      download.cactus.results,
+      path = filepath,
+      chemical.format = "SDF"
+    )
+  ) 
+
+# print(results.dwnld)
+# View(results.dwnld)
